b86075b7aa9455e910fa0cb42827d7f037aa91c8
Karin <karinlevi204@gmail.com>
2017-03-24 12:22:47 +0300

    added gps location added sms - not finished

---
+++ app/src/main/java/com/example/karin/diabeatis/mapHelpFregment.java
@@ -0,0 +1,198 @@
+package com.example.karin.diabeatis;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.Application;
+import android.app.Fragment;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.telephony.SmsManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * Created by Karin on 23/03/2017.
+ */
+
+public class mapHelpFregment extends Fragment implements OnMapReadyCallback {
+
+    private LocationManager locationManager;
+    private final String TAG = mapHelpFregment.class.getSimpleName();
+    private Application activity;
+    private MapView mMapView;
+    private GoogleMap mMap;
+    private String number;
+    private String message;
+    private double longitude;
+    private double latitude;
+    private TextView txtAddress;
+    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS =0 ;
+
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View v = inflater.inflate(R.layout.fregment_map, container, false);
+        activity = this.getActivity().getApplication();
+        txtAddress = (TextView)v.findViewById(R.id.txtAdd);
+       // cellNumber = Long.parseLong(details);
+        number = getArguments().getString("Number");
+        latitude = getArguments().getDouble("Latitude");
+        longitude = getArguments().getDouble("Longitude");
+        locationManager = (LocationManager) this.getActivity().getApplication().getSystemService(Context.LOCATION_SERVICE);
+        if ( !locationManager.isProviderEnabled( LocationManager.GPS_PROVIDER ) ) {
+            buildAlertMessageNoGps();
+        }
+        mMapView = (MapView) v.findViewById(R.id.map);
+        mMapView.onCreate(savedInstanceState);
+        mMapView.getMapAsync(this);
+
+        return v;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap)
+    {
+        mMap = googleMap;
+        Geocoder geocoder;
+        List<Address> addresses = null;
+
+        geocoder = new Geocoder(activity, Locale.getDefault());
+
+
+        if(latitude != 0 && longitude != 0) {
+            try {
+                addresses = geocoder.getFromLocation(latitude, longitude, 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+                }catch (IOException e) {
+                e.printStackTrace();}
+
+            if (addresses != null && addresses.size() > 0) {
+                final String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+                final String city = addresses.get(0).getLocality();
+                final String country = addresses.get(0).getCountryName();
+
+                mMap.addMarker(new MarkerOptions().snippet(address + ", " + city).position(new LatLng(latitude, longitude)));
+                txtAddress.setText("Current Address: " + address +" "+city);
+                message = "check "+ address+" "+city;
+                sendSMSMessage();
+
+
+                CameraPosition cameraPosition = new CameraPosition.Builder().target(new LatLng(latitude, longitude)).zoom(12).build();
+                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            }
+        }
+
+        if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this.getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
+        }
+        mMap.setMyLocationEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+        mMap.getUiSettings().setCompassEnabled(true);
+    }
+
+
+    protected void sendSMSMessage() {
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.SEND_SMS)) {
+            }
+            else
+            {
+                ActivityCompat.requestPermissions(getActivity(),new String[]{Manifest.permission.SEND_SMS},
+                        MY_PERMISSIONS_REQUEST_SEND_SMS);
+            }
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_SEND_SMS: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    SmsManager smsManager = SmsManager.getDefault();
+                    smsManager.sendTextMessage(number, null, message, null, null);
+                    Toast.makeText(activity, "SMS sent.",
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(activity,
+                            "SMS faild, please try again.", Toast.LENGTH_LONG).show();
+                    return;
+                }
+            }
+        }
+    }
+
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
+    }
+
+
+    private void buildAlertMessageNoGps() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage("Your GPS seems to be disabled, do you want to enable it?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        dialog.cancel();
+
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
+    }
+}
--- app/src/main/java/com/example/karin/diabeatis/startPage.java
+++ app/src/main/java/com/example/karin/diabeatis/startPage.java
@@ -1,18 +1,189 @@
 package com.example.karin.diabeatis;
 
+import android.Manifest;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 
-public class startPage extends AppCompatActivity {
+import com.google.android.gms.maps.MapFragment;
+
+import java.util.Calendar;
+
+public class startPage extends AppCompatActivity implements View.OnClickListener,LocationListener {
 
     private Button btnHelp;
+    private String number = "0528840354";
+    private LocationManager locationManager;
+    private Location location;
+    private long startTime = 0;
+    private double longitude;
+    private double latitude;
+    private final String TAG = startPage.class.getSimpleName();
+
+
+
+    Handler timerHandler = new Handler();
+    Runnable timerRunnable = new Runnable() {
+        @Override
+        public void run() {
+            long millis = startTime;
+            int seconds = (int) (millis / 1000);
+            int minutes = seconds / 60;
+            seconds = seconds % 60;
+            startTime += 1000;
+            timerHandler.postDelayed(this, 1000);
+        }
+    };
+
+
+    Handler gpsPosHandler = new Handler();
+    Runnable selfPositionThred = new Runnable() {
+        @Override
+        public void run() {
+            if (ActivityCompat.checkSelfPermission(startPage.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(startPage.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(startPage.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
+                return;
+            }
+            location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (location != null && location.getTime() > Calendar.getInstance().getTimeInMillis() - 2 * 60 * 1000) {
+                Log.d(TAG, "Location obtained");
+            } else {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, startPage.this);
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnHelp = (Button)findViewById(R.id.btnHelp);
+        btnHelp.setOnClickListener(this);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        startTime = 0;
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
+        }
+        locationManager.removeUpdates(this);
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        int id = v.getId();
+
+        switch(id)
+        {
+            case R.id.btnHelp:
+            {
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                mapHelpFregment f = new mapHelpFregment();
+                if(location != null)
+                {
+                    latitude = location.getLatitude();
+                    longitude = location.getLongitude();
+                }
+                else
+                {
+                    latitude = 0.0;
+                    longitude = 0.0;
+                }
+
+                Bundle bundle = new Bundle();
+                btnHelp.setVisibility(View.INVISIBLE);
+                bundle.putInt("ActivityId", R.id.activity_main);
+                bundle.putDouble("Latitude",latitude);
+                bundle.putDouble("Longitude",longitude);
+                bundle.putString("Number",number);
+                f.setArguments(bundle);
+                fragmentTransaction.replace(R.id.activity_main, f);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        }
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (!locationManager.isProviderEnabled( LocationManager.GPS_PROVIDER ) ) {
+            buildAlertMessageNoGps();
+        }
+        else {
+            gpsPosHandler.postDelayed(selfPositionThred, 0);
+            timerHandler.postDelayed(timerRunnable, 0);
+        }
+
 
     }
+
+    @Override
+    public void onLocationChanged(Location location) { if (location != null) {
+        Log.v("Location Changed", location.getLatitude() + " and " + location.getLongitude());
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
+        }
+        locationManager.removeUpdates(this);
+    }
+
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+    @Override
+    public void onProviderEnabled(String provider) {}
+
+    @Override
+    public void onProviderDisabled(String provider) {}
+
+    private void buildAlertMessageNoGps() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Your GPS seems to be disabled, do you want to enable it?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        dialog.cancel();
+                        timerHandler.postDelayed(timerRunnable, 0);
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+
+
 }
